80917a7a4072280640d8ed7bf8b313597532d9c2
JalexChang <jalex.cpc@gmail.com>
2017-08-24 14:17:11 +0800

    add stack_range to factor_dist_on_users / factor_dist_on_items

--- src/mta/profile.py
+++ src/mta/profile.py
@@ -37,49 +37,49 @@ class Profile:
 
     #get the distribution of number of touched factors in each users
     @classmethod
-    def factor_dist_on_users(self, dataset):
-        matrix_shape = ( dataset.matrix_shape()[0],dataset.matrix_shape()[1])
-        user_num_factors = numpy.zeros(matrix_shape[0], dtype=int)
-        dist = numpy.zeros(matrix_shape[1], dtype=int)
+    def factor_dist_on_users(self, dataset, stacks=21, stack_range=1):
+        user_size = dataset.matrix_shape()[0]
+        factor_size = dataset.matrix_shape()[1]
+        user_num_factors = numpy.zeros(user_size, dtype=int)
+        dist = numpy.zeros(stacks, dtype=int)
+        labels = numpy.array(list( i*stack_range for i in range(stacks)))
 
         user_factor_matrix = dataset.touchs.to_matrix()
-        for u_id in range(matrix_shape[1]):
+        for u_id in range(user_size):
             user_num_factors[u_id] = user_factor_matrix[u_id].sum()
-
         for num_factor in user_num_factors:
-            dist[num_factor] +=1
-
-        for index in reversed(range(len(dist))):
-            if dist[index] >0:
-                dist = dist[:index+1]
-                break
-        return dist
+            if num_factor >= stacks*stack_range:
+                dist[stacks-1] +=1
+            else :
+                index = math.floor(num_factor/stack_range)
+                dist[index] +=1
+        return dist, labels
 
     #get the distribution of number of touched factors in each items 
     @classmethod
-    def factor_dist_on_items(self, dataset):
-        matrix_shape = ( dataset.matrix_shape()[2],dataset.matrix_shape()[1])
-        item_factor_table  = numpy.zeros(matrix_shape, dtype=bool)
-        item_num_factors = numpy.zeros(matrix_shape[0], dtype=int)
-        dist = numpy.zeros(matrix_shape[1], dtype=int)
+    def factor_dist_on_items(self, dataset,  stacks=21, stack_range=1):
+        item_size = dataset.matrix_shape()[2]
+        factor_size = dataset.matrix_shape()[1]
+        item_factor_table  = numpy.zeros((item_size, factor_size), dtype=bool)
+        item_num_factors = numpy.zeros(item_size, dtype=int)
+        dist = numpy.zeros(stacks, dtype=int)
+        labels = numpy.array(list( i*stack_range for i in range(stacks)))
+        
         user_factor_matrix = dataset.touchs.to_matrix()
-
         for u_id, i_id, rating in dataset.ratings.to_list():
-            for f_id in range(matrix_shape[1]):
+            for f_id in range(factor_size):
                 if user_factor_matrix[u_id][f_id] ==1:
                     item_factor_table[i_id][f_id] =True
-
         for i_id in range(len(item_factor_table)):
             item_num_factors[i_id] = item_factor_table[i_id].sum()
 
         for num_factor in item_num_factors:
-            dist[num_factor] +=1
-
-        for index in reversed(range(len(dist))):
-            if dist[index] >0:
-                dist = dist[:index+1]
-                break
-        return dist
+            if num_factor >= stacks*stack_range:
+                dist[stacks-1] +=1
+            else :
+                index = math.floor(num_factor/stack_range)
+                dist[index] +=1
+        return dist, labels
 
     #get the distribution of number of products in each factors
     @classmethod
